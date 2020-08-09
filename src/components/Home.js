b6f1b62ea5aefc4dbcfd8a4e6b48c609c06fb98a
Elizabeth Soto <elizabeths87@icloud.com>
2020-08-08 21:13:49 -0700

    removed unused-anchor-tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
         return (
             <section className="header-content">
               <div className="header-text">
-               <h1>Mexican Inspired Pastries.<a href="/"></a></h1>
+               <h1>Mexican Inspired Pastries.</h1>
              </div>  
       </section>
         )
